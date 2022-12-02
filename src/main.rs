17f56be08a6d1b235bb7f6d6b2c86f8ebcd4df02
Nathan Friedly <nathan@nfriedly.com>
2022-12-02 11:14:49 -0500

    Part two

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 enum RPS {
     Rock,
     Paper,
     Scissors,
 }
 
-use RPS::*;
+#[derive(Debug)]
+enum Outcome {
+    Win,
+    Draw,
+    Loose,
+}
 
-fn parse_line(line: &str) -> (RPS, RPS) {
+use RPS::*;
+use Outcome::*;
+
+fn parse_line(line: &str) -> (RPS, Outcome) {
     let mut iter = line.split_whitespace();
     let left = match iter.next() {
         Some("A") => Rock,
@@ -16,39 +24,49 @@ fn parse_line(line: &str) -> (RPS, RPS) {
         _ => panic!("failed to parse first letter"),
     };
     let right = match iter.next() {
-        Some("X") => Rock,
-        Some("Y") => Paper,
-        Some("Z") => Scissors,
+        Some("X") => Loose,
+        Some("Y") => Draw,
+        Some("Z") => Win,
         _ => panic!("failed to parse second letter"),
     };
     (left, right)
 }
 
-fn parse(input: &str) -> Vec<(RPS, RPS)> {
-    let mut parsed: Vec<(RPS, RPS)> = Vec::new();
+fn parse(input: &str) -> Vec<(RPS, Outcome)> {
+    let mut parsed: Vec<(RPS, Outcome)> = Vec::new();
     for line in input.lines() {
         parsed.push(parse_line(line));
     }
     parsed
 }
 
-fn score_round(round: (RPS, RPS)) -> usize {
-    let mut points = 0;
-    points += match round {
-        (_, Rock) => 1,
-        (_, Paper) => 2,
-        (_, Scissors) => 3,
+fn score_round(round: (RPS, Outcome)) -> usize {
+    let my_hand = match round {
+        (anything, Draw) => anything,
+        (Rock, Loose) => Scissors,
+        (Rock, Win) => Paper,
+        (Paper, Loose) =>  Rock,
+        (Paper, Win) => Scissors,
+        (Scissors, Loose) => Paper,
+        (Scissors, Win) => Rock,
     };
-    points += match round {
-        (Paper, Rock) | (Scissors, Paper) | (Rock, Scissors) => 0,
-        (Rock, Rock) | (Paper, Paper) | (Scissors, Scissors) => 3,
-        (Rock, Paper) | (Paper, Scissors) | (Scissors, Rock) => 6,
+    let wld_points = match round {
+        (_, Loose) => 0,
+        (_, Draw) => 3,
+        (_, Win) => 6
     };
-    println!("score for {:?} is {}", round, points);
+    let shape_points = match my_hand {
+        Rock => 1,
+        Paper => 2,
+        Scissors => 3,
+    };
+    let points = wld_points + shape_points;
+
+    println!("response for {:?} is {:?} for {} points", round, my_hand, points);
     points
 }
 
-fn score(rounds:  Vec<(RPS, RPS)>) -> usize {
+fn score(rounds:  Vec<(RPS, Outcome)>) -> usize {
     let mut points = 0;
     for round in rounds {
         points += score_round(round);
